986af38d771bbd9cd1b7348e5dd90d1512c456a6
Crazy Ape Coin <85198323+solcrazyape@users.noreply.github.com>
2021-10-28 13:55:39 -0700

    Update StandaloneBalancesDisplay.tsx

--- src/components/StandaloneBalancesDisplay.tsx
+++ src/components/StandaloneBalancesDisplay.tsx
@@ -35,8 +35,8 @@ const Tip = styled.p`
 `;
 
 const ActionButton = styled(Button)`
-  color: #2abdd2;
-  background-color: #212734;
+  color: #D55FEF;
+  background-color: #860EA1;
   border-width: 0px;
 `;
 
@@ -198,7 +198,7 @@ export default function StandaloneBalancesDisplay() {
                   title="Token mint"
                   trigger="hover"
                 >
-                  <InfoCircleOutlined style={{ color: '#2abdd2' }} />
+                  <InfoCircleOutlined style={{ color: '#D55FEF' }} />
                 </Popover>
               )}
             </Divider>
